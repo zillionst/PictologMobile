b8b85717566b54b6f4dcb8c6acf190bcf119d09c
JONGPARK <azznggu@gmail.com>
2017-01-09 19:02:39 +0900

    updated

--- app/src/main/java/com/zillionstar/pictologm/MainActivity.java
+++ app/src/main/java/com/zillionstar/pictologm/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String name = "hello Android!";
+    private String name = "Hello Ji sung Kwon!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
