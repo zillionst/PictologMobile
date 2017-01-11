808046afee854bac40d4bc224223aaa83fc91b43
JONGPARK <azznggu@gmail.com>
2017-01-11 16:37:11 +0900

    COMMIT

--- app/src/main/java/com/zillionstar/pictologm/MainActivity.java
+++ app/src/main/java/com/zillionstar/pictologm/MainActivity.java
@@ -1,7 +1,7 @@
 package com.zillionstar.pictologm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
@@ -24,23 +24,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void mOnClick(View v) {
-        switch(v.getId()) {
-            case R.id.btn_home :
+        switch (v.getId()) {
+            case R.id.btn_home:
                 mWeb.loadUrl("http://10.0.2.2:8888/Pictolog/pictolog");
                 break;
-            case R.id.btn_search :
+            case R.id.btn_search:
 
                 break;
-            case R.id.btn_add :
+            case R.id.btn_add:
 
                 break;
-            case R.id.btn_private :
-
+            case R.id.btn_private:
+                
                 break;
-            case R.id.btn_setting :
+            case R.id.btn_setting:
                 mWeb.loadUrl("http://10.0.2.2:8888/Pictolog/memberSetting");
                 break;
-            default :
+            default:
                 break;
         }
     }
@@ -49,9 +49,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
             view.loadUrl(request.getUrl().toString());
-
             return true;
         }
     }
+
 
 }
